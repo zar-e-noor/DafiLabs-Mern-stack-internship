e81fa6a5de9e0132dab1b67a5cee52136346d28b
Noor Fatima <noorfatimasohail55@gmail.com>
2026-07-17 23:37:20 +0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // Allows production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Allows production builds to successfully complete even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
